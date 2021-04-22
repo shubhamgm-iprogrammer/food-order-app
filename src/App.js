f94d4d0dbab03623eec9cdeeb10724c3a767a586
shubhamgm-iprogrammer <shubhamgm.iprogrammer@gmail.com>
2021-04-22 19:02:21 +0530

    food-order-app-sec-11

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
+function App() {
+  return (
+    <React.Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
